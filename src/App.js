ee99d0c0e33d7577a377475fd673294e272dacf3
pcchamikara <pcchamikara@gmail.com>
2021-08-07 14:40:22 +0530

    added basic routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import FormComponent from "./Components/formcomponent";
 import "./styles.css";
 import AppBar from "@material-ui/core/AppBar";
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Toolbar from "@material-ui/core/Toolbar";
+import Preferences from './Pages/Preferences';
+import Dashboard from './Pages/Dashboard';
+import Login from './Components/Login/Login';
 
 export default function App() {
   return (
@@ -9,7 +13,25 @@ export default function App() {
       <AppBar position="fixed">
         <Toolbar></Toolbar>
       </AppBar>
-      <FormComponent />
+      <BrowserRouter>
+      <Switch>
+      <Route path="/register">
+        <FormComponent />
+      </Route>
+      <Route path="/dashboard">
+            <Dashboard />
+          </Route>
+          <Route path="/preferences">
+            <Preferences />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+      </Switch>
+      </BrowserRouter>
+      
+     
+    
     </div>
   );
 }
